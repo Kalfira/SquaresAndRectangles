a780b49283ecb332c397d1c623856846bd929751
Plourde268 <jst268@outlook.com>
2015-07-07 16:47:54 -0500

    reddit api

---
+++ Zed.Presentation.Web/Zed.Domain/Models/RedditsSearch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zed.Domain.Models
+{
+    [Serializable()]
+    public class redditsSearchItem
+    {
+        [System.Xml.Serialization.XmlElement("title")]
+        public string Title { get; set; }
+
+        [System.Xml.Serialization.XmlElement("link")]
+        public string Link { get; set; }
+
+        [System.Xml.Serialization.XmlElement("guid")]
+        public string Guid { get; set; }
+
+        [System.Xml.Serialization.XmlElement("dc:date")]
+        public string PublishDate { get; set; }
+
+        [System.Xml.Serialization.XmlElement("description")]
+        public string Description { get; set; }
+    }
+
+
+    [Serializable()]
+    public class redditsSearchChannel
+    {
+        [System.Xml.Serialization.XmlElement("title")]
+        public string Title { get; set; }
+
+        [System.Xml.Serialization.XmlElement("link")]
+        public string Link { get; set; }
+
+        [System.Xml.Serialization.XmlElement("description")]
+        public string Description { get; set; }
+
+
+
+        [System.Xml.Serialization.XmlElement("item")]
+        public List<redditsSearchItem> item { get; set; }
+    }
+
+
+    [Serializable()]
+    [System.Xml.Serialization.XmlRoot("rss")]
+    public class redditsSearch
+    {
+        [System.Xml.Serialization.XmlElement("channel")]
+        public redditsSearchChannel channel { get; set; }
+    }
+}
