49f83d437872dd9d60621d75b4bbefec85f1c631
Kalfira <zdegner@gmail.com>
2015-07-14 07:35:42 -0500

    Merges suck

---
+++ SquaresandRectangles/SquaresandRectangles.Service/Services/Reddit/Models/About.cs
@@ -0,0 +1,24 @@
+﻿namespace SquaresandRectangles.Service.Modules.Reddit.Models
+{
+    public class redditAboutData
+    {
+        public bool has_mail { get; set; }
+        public string name { get; set; }
+        public bool is_friend { get; set; }
+        public double created { get; set; }
+        public string modhash { get; set; }
+        public double created_utc { get; set; }
+        public int link_karma { get; set; }
+        public int comment_karma { get; set; }
+        public bool is_gold { get; set; }
+        public bool is_mod { get; set; }
+        public string id { get; set; }
+        public bool has_mod_mail { get; set; }
+    }
+
+    public class redditAbout
+    {
+        public string kind { get; set; }
+        public redditAboutData data { get; set; }
+    }
+}
---
+++ SquaresandRectangles/SquaresandRectangles.Service/Services/Reddit/Models/RedditDemo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace SquaresandRectangles.Service.Modules.Reddit.Models
+{
+    public class RedditDemo
+    {
+        public RedditData data { get; set; }
+    }
+
+    public class RedditData
+    {
+        public IList<RedditChildren> children { get; set; }
+    }
+
+    public class RedditChildren
+    {
+        public string name { get; set; }
+        public IList<string> mod_permissions { get; set; }
+    }
+}
